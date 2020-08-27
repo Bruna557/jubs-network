eb82297adb67e37bccc084e237e28641a38b1a47
Bruna Schroder <bruna.schroder@eldorado.org.br>
2020-08-27 19:09:26 -0300

    Allow all methods on cors config

--- api/src/main/java/com/github/Bruna557/restapi/Application.java
+++ api/src/main/java/com/github/Bruna557/restapi/Application.java
@@ -20,6 +20,7 @@ public class Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
                         .allowCredentials(true);
             }
         };
